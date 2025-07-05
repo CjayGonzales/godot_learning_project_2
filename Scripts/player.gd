77b3659e6b9d5187cdd05a46a75d4bbc20e57813
Clemente Gonzales <cjaygonzales117@gmail.com>
2025-07-05 09:38:47 +0100

    added a basic double jump mechanic

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,9 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
 @onready var area_2d: Area2D = $Area2D
 
+var double_jump = 2
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,8 +13,16 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		print(double_jump)
+		if double_jump == 2 || 1:
+			velocity.y = JUMP_VELOCITY
+			double_jump -= 1
+			if double_jump == 0 || double_jump < 0:
+				velocity.y = 0
+		if is_on_floor():
+			double_jump = 2
+			
 		
 	# Handle Click
 	if Input.is_action_just_pressed("left_click"):
