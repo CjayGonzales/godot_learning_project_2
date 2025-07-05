7cb45d021dd441be14aa8336c274a07f37863b97
Clemente Gonzales <cjaygonzales117@gmail.com>
2025-07-05 10:10:01 +0100

    fixed double jump bugs using grounded, jump, and mid-air states

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,24 +5,19 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 @onready var area_2d: Area2D = $Area2D
 
-var double_jump = 2
+var double_jump = "grounded"
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	if is_on_floor():
+		double_jump = "grounded"
+	# Handle jump.	
 	if Input.is_action_just_pressed("ui_accept"):
-		print(double_jump)
-		if double_jump == 2 || 1:
-			velocity.y = JUMP_VELOCITY
-			double_jump -= 1
-			if double_jump == 0 || double_jump < 0:
-				velocity.y = 0
-		if is_on_floor():
-			double_jump = 2
-			
+		double_jump_function()
+	
 		
 	# Handle Click
 	if Input.is_action_just_pressed("left_click"):
@@ -42,3 +37,18 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	pass # Replace with function body.
+
+func double_jump_function():
+	print(double_jump)
+	
+	if double_jump == "grounded":
+		velocity.y = JUMP_VELOCITY
+		double_jump = "first_jump"
+		
+	elif double_jump == "first_jump":
+		velocity.y = JUMP_VELOCITY - 50
+		double_jump = "second_jump"
+		
+	if double_jump == "second_jump":
+		pass
+	
